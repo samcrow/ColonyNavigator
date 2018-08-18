c2526a5cefbc669587af19b441f85b3243c47833
Sam Crow <scrow@eng.ucsd.edu>
2018-08-18 15:48:10 -0700

    Experimental UTM-based transformation, which does not work correctly

--- app/src/main/java/org/samcrow/colonynavigator/CoordinateTransformer.java
+++ app/src/main/java/org/samcrow/colonynavigator/CoordinateTransformer.java
@@ -5,6 +5,9 @@ import android.graphics.PointF;
 
 import org.mapsforge.core.model.LatLong;
 
+import uk.me.jstott.jcoord.LatLng;
+import uk.me.jstott.jcoord.UTMRef;
+
 /**
  * Transforms coordinates from GPS latitude/longitude into local colony
  * coordinates
@@ -13,26 +16,37 @@ import org.mapsforge.core.model.LatLong;
  */
 public class CoordinateTransformer {
 
-    private static final String TAG = CoordinateTransformer.class.getName();
     private static CoordinateTransformer instance;
-    private Matrix matrix = new Matrix();
-    private Matrix inverse = new Matrix();
+
+    /*
+     * How this works:
+     * Colony coordinates <-> UTM coordinates (zone 12N) <-> latitude/longitude
+     */
+
+    /**
+     * Matrix mapping from homogenous colony coordinates to homogenous UTM coordinates
+     */
+    private final Matrix mColonyToUtm;
+    /**
+     * Matrix mapping from homogenous UTM coordinates to homogenous UTM coordinates
+     */
+    private final Matrix mUtmToColony;
 
     /**
      * Constructor
      */
     private CoordinateTransformer() {
-
-        // Hard-coded values provided by Erik Steiner
-        inverse.setValues(new float[]{
-                0.0000031740000f, 0.0000006358000f, -109.043098200f,
-                -0.0000005184000f, 0.0000027130000f, 31.870789500f,
-                0.0f, 0.0f, 1.0f
+        mColonyToUtm = new Matrix();
+        mColonyToUtm.setValues(new float[] {
+                1.02062179523250e+04f,  -3.86246861418180e+03f,   2.58912843430477e+06f,
+                5.25180433326394e+04f,  -1.98904950624002e+04f,   1.33356990408822e+07f,
+                1.48869418695798e-02f,  -5.63826502026197e-03f,   3.78016445511664e+00f,
         });
-        matrix.setValues(new float[]{
-                303445, -71113.3f, 3.5355E7f,
-                57982.3f, 355007, -4.99179E6f,
-                0.0f, 0.0f, 1.0f
+        mUtmToColony = new Matrix();
+        mUtmToColony.setValues(new float[] {
+                1.10574743324865e-01f,   3.29840736957084e-01f,  -1.23935058042237e+06f,
+                1.19897446505948e-01f,   4.73794368925693e+00f,  -1.67966821958193e+07f,
+                -2.56630686041893e-04f,   5.76786077766980e-03f,  -2.01718752027190e+04f,
         });
     }
 
@@ -53,13 +67,13 @@ public class CoordinateTransformer {
      * @return A point with the transformed coordinates
      */
     public PointF toLocal(double longitude, double latitude) {
-        double x = longitude;
-        double y = latitude;
-        float[] points = new float[]{(float) x, (float) y};
-
-        matrix.mapPoints(points);
-
-        return new PointF(points[0], points[1]);
+        // Part 1: Latitude/longitude to UTM
+        final LatLng ll = new LatLng(latitude, longitude);
+        final UTMRef utm = ll.toUTMRef();
+        // Part 2: UTM to colony coordinates
+        final float[] coordinates = new float[] {(float) utm.getEasting(), (float) utm.getNorthing()};
+        mUtmToColony.mapPoints(coordinates);
+        return new PointF(coordinates[0], coordinates[1]);
     }
 
     /**
@@ -71,12 +85,15 @@ public class CoordinateTransformer {
      * y
      */
     public LatLong toGps(float x, float y) {
+        // Part 1: Colony to UTM
+        final float[] utm = new float[] {x, y};
+        mColonyToUtm.mapPoints(utm);
+        final float easting = utm[0];
+        final float northing = utm[1];
 
-        float[] points = new float[]{x, y};
-
-        inverse.mapPoints(points);
-
-        return new LatLong(points[1], points[0]);
-
+        // Part 2: UTM to latitude/longitude
+        final UTMRef utmRef = new UTMRef(easting, northing, 'N', 12);
+        final LatLng ll = utmRef.toLatLng();
+        return new LatLong(ll.getLat(), ll.getLng());
     }
 }
